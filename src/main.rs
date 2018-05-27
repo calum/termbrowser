0032f4deab9844b09637bb1cda356acae545a241
Tpt <thomas@pellissier-tanon.fr>
2018-05-27 12:56:04 +0200

    display

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,16 @@ mod img;
 
 use actix_web::{server, App, HttpRequest, Responder};
 use ansi_term::Color::Red;
+use img::print_image;
 
-fn greet(req: HttpRequest) -> impl Responder {
-    let to = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!\n", Red.paint(to))
+fn display(req: HttpRequest) -> impl Responder {
+    let image = image::open("imgs/happy_ferris.pgn").unwrap();
+    print_image(image, true, 100, 100)
 }
 
 fn main() {
-    server::new(|| {
-        App::new()
-            .resource("/", |r| r.f(greet))
-            .resource("/{name}", |r| r.f(greet))
-    }).bind("127.0.0.1:8000")
+    server::new(|| App::new().resource("/", |r| r.f(display)))
+        .bind("127.0.0.1:8000")
         .expect("Can not bind to port 8000")
         .run();
 }
