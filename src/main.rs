09089facf643b14dee16d1a896a20f2d50b3946b
Tpt <thomas@pellissier-tanon.fr>
2018-05-27 12:27:01 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+extern crate actix_web;
+extern crate ansi_term;
+
+use actix_web::{server, App, HttpRequest, Responder};
+use ansi_term::Color::Red;
+
+fn greet(req: HttpRequest) -> impl Responder {
+    let to = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!\n", Red.paint(to))
+}
+
+fn main() {
+    server::new(|| {
+        App::new()
+            .resource("/", |r| r.f(greet))
+            .resource("/{name}", |r| r.f(greet))
+    }).bind("127.0.0.1:8000")
+        .expect("Can not bind to port 8000")
+        .run();
+}
